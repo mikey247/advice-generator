7443f9a46c28ddd478bd4d8e24432d4d9e88f22e
Michael Umeokoli <michaeljunior794@gmail.com>
2022-03-03 21:22:34 +0100

    'added-spinner'

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const url = "https://api.adviceslip.com/advice";
+// document.querySelector(".loader").style.display = "none";
 
-const getAdvice = function () {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      document.querySelector(".advice-text").textContent = data.slip.advice;
-      document.querySelector(".advice-number").textContent = data.slip.id;
-    });
+const motherFunction = function () {
+  const getAdvice = function () {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        document.querySelector(".advice-text").textContent = data.slip.advice;
+        document.querySelector(".advice-number").textContent = data.slip.id;
+      });
+  };
+
+  document.querySelector(".container").style.display = "none";
+  document.querySelector(".parent").classList.add("spinner-1");
+
+  setTimeout(() => {
+    document.querySelector(".parent").classList.remove("spinner-1");
+    document.querySelector(".container").style.display = "block";
+    getAdvice();
+  }, 2000);
 };
+motherFunction();
 
-getAdvice();
-
-document.querySelector(".dice-button").addEventListener("click", getAdvice);
+document
+  .querySelector(".dice-button")
+  .addEventListener("click", motherFunction);
